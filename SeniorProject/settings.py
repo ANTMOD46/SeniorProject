9238ed7cd3b83d3c169d217af51445a29b060e03
ANTMOD46 <Juntarisil55@gmail.com>
2025-01-17 22:08:50 +0700

    media

--- SeniorProject/settings.py
+++ SeniorProject/settings.py
@@ -89,6 +89,8 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -164,8 +166,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "theme/static"]
 
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
